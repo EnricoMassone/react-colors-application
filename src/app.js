770e0eb2f06bb02908824b0c801c11d766e02bf3
Enrico Massone <enrico.massone@deltatre.it>
2020-01-18 12:17:39 +0100

    work in progress: adding state to the app component

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { render } from "react-dom";
+import React, { Component } from "react";
 import AddColorForm from "./addColorForm";
 
-const App = () => {
-  return (
-    <React.StrictMode>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: []
+    };
+
+    this.addNewColor = this.addNewColor.bind(this);
+  }
+
+  addNewColor(title, hexCode) {
+    console.log(title, hexCode);
+  }
+
+  render() {
+    const { colors } = this.state;
+
+    return (
       <div>
         <header>
           <h1>Colors manager</h1>
         </header>
         <main>
           <AddColorForm onColorAdded={f => f} />
+          <pre>{JSON.stringify(colors, null, 2)}</pre>
         </main>
       </div>
-    </React.StrictMode>
-  );
-};
+    );
+  }
+}
 
-const root = document.getElementById("root");
-render(<App />, root);
+export default App;
